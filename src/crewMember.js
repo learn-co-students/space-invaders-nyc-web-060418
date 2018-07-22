1f3791fecd4abe44684dc78be21ebae588cb8780
Ms-Alex <ms.hernandeza1@gmail.com>
2018-07-21 23:18:28 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,30 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position){
+    this.position = position
+    this.currentShip = "Looking for a Rig"
+  }
+
+  engageWarpDrive(){
+    if (this.currentShip !== "Looking for a Rig" && this.position = 'Pilot') {
+      return this.currentShip.warpDrive = "engaged"
+    }
+    return "had no effect"
+
+  }
+
+  setsInvisibility(){
+    if (this.currentShip !== "Looking for a Rig" && this.position === 'Defender') {
+      return this.currentShip.cloaked = true
+    }
+    return "had no effect"
+
+  }
+
+  chargePhasers(){
+    if (this.currentShip !== "Looking for a Rig" && this.position === 'Gunner') {
+      return this.currentShip.phasersCharge = "charged"
+    }
+    return "had no effect"
+  }
+
+}
